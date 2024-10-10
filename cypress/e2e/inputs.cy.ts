b0600eeeadc59806fab16077247bf498771ae539
Michael Pearson <mfpears@gmail.com>
2024-10-09 20:06:46 -0600

    Refactor from unit to path tests

--- cypress/e2e/inputs.cy.ts
+++ cypress/e2e/inputs.cy.ts
@@ -1,64 +1,47 @@
-/// <reference types="cypress" />
+const sel = (id: string) => `[data-test=${id}]`;
+
+const sourceAmount = sel('cashSource-amount');
+const targetAmount = sel('cashTarget-amount');
+const exchangeRateForced = sel('exchangeRateForced');
+const switchCurrencies = sel('switch');
+const saveCurrencies = sel('save');
 
 describe('exchange rate app inputs', () => {
     beforeEach(() => {
         cy.visit('http://localhost:4200');
+        localStorage.removeItem('history');
     });
 
-    it('can change source currency to AED', () => {
+    it('can change currencies, enter source, derive target, save and display card', () => {
         cy.contains('EUR').click();
         cy.contains('AED').click();
-        cy.contains('EUR').should('not.exist');
-        cy.contains('AED').should('exist');
-    });
-
-    it('can change target currency to AED', () => {
         cy.contains('USD').click();
-        cy.contains('AED').click();
-        cy.contains('USD').should('not.exist');
-        cy.contains('AED').should('exist');
-    });
-
-    const sel = (id: string) => `[data-test=${id}]`;
-
-    const sourceAmount = sel('cashSource-amount');
-    const targetAmount = sel('cashTarget-amount');
-    const exchangeRateForced = sel('exchangeRateForced');
-    const switchCurrencies = sel('switch');
-
-    it('should switch currencies only', () => {
+        cy.contains('TWD').click();
         cy.get(sourceAmount).type('5');
         cy.get(switchCurrencies).click();
         cy.get(sourceAmount).should('have.value', '5');
-        cy.get(sourceAmount).parent().contains('USD');
-        cy.get(targetAmount).parent().contains('EUR');
+        cy.get(targetAmount).should('have.value', '5.50');
+        cy.get(sourceAmount).parent().contains('TWD');
+        cy.get(targetAmount).parent().contains('AED');
+
+        cy.get(saveCurrencies).click();
     });
 
-    it('should show target amount as source * exchange rate', () => {
-        cy.get(sourceAmount).type('100');
-        cy.get(targetAmount).should('have.value', '110.00');
-    });
-
-    it('should show source amount as target / exchange rate', () => {
+    it('should show source amount as target / exchange rate and target as source * exchange rate', () => {
         cy.get(targetAmount).type('11');
         cy.get(sourceAmount).should('have.value', '10.00');
+        cy.get(exchangeRateForced).type('1.11');
+        cy.contains('Forced Exchange Rate: 1.11');
+        cy.get(targetAmount).should('have.value', '11.10');
+
+        cy.get(saveCurrencies).click();
     });
 
-    it('should show target amount as source * exchange rate after custom exchange rate', () => {
-        cy.get(sourceAmount).type('100');
-        cy.get(exchangeRateForced).type('299');
-        cy.get(targetAmount).should('have.value', '29900.00');
-    });
-
-    it('should show forced exchanged rate', () => {
-        cy.get(exchangeRateForced).type('299');
-        cy.contains('Forced Exchange Rate: 299');
-    });
-
-    it('should show error for extreme forced exchange rate', () => {
+    it('should show error for extreme forced exchange rate and disable saving', () => {
         cy.get(exchangeRateForced).type('299');
         cy.get(exchangeRateForced).blur();
         cy.contains('ER variation > 2% ER limit!');
+        cy.get(saveCurrencies).should('be.disabled');
     });
 
     it('should show target amount as source * randomly generated exchange rate', () => {
