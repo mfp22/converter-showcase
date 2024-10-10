ba5cbdcd246df5b34ab5ff73c2dd035372f23d45
Michael Pearson <mfpears@gmail.com>
2024-10-10 00:34:54 -0600

    Complete tests 1.0

--- cypress/e2e/inputs.cy.ts
+++ cypress/e2e/inputs.cy.ts
@@ -5,11 +5,14 @@ const targetAmount = sel('cashTarget-amount');
 const exchangeRateForced = sel('exchangeRateForced');
 const switchCurrencies = sel('switch');
 const saveCurrencies = sel('save');
+const exchangeRateCard = sel('exchangeRateCard');
+const exchangeRateCardRealRate = `${exchangeRateCard} ${sel('realRate')}`;
+const exchangeRateCardForcedRate = `${exchangeRateCard} ${sel('forcedRate')}`;
+const exchangeRateCardDelete = `${exchangeRateCard} ${sel('delete')}`;
 
 describe('exchange rate app inputs', () => {
     beforeEach(() => {
         cy.visit('http://localhost:4200');
-        localStorage.removeItem('history');
     });
 
     it('can change currencies, enter source, derive target, save and display card', () => {
@@ -17,6 +20,8 @@ describe('exchange rate app inputs', () => {
         cy.contains('AED').click();
         cy.contains('USD').click();
         cy.contains('TWD').click();
+        cy.contains('AED');
+        cy.contains('TWD');
         cy.get(sourceAmount).type('5');
         cy.get(switchCurrencies).click();
         cy.get(sourceAmount).should('have.value', '5');
@@ -25,6 +30,25 @@ describe('exchange rate app inputs', () => {
         cy.get(targetAmount).parent().contains('AED');
 
         cy.get(saveCurrencies).click();
+        cy.get(saveCurrencies).should(() => {
+            expect(localStorage.getItem('history')).to.contain('targetValue').and.to.contain('5.50');
+        });
+        cy.get(exchangeRateCard)
+            .contains('TWD')
+            .should($cardTitle => {
+                const titleText = $cardTitle.text();
+                expect(titleText.indexOf('TWD')).to.be.lessThan(titleText.indexOf('AED'));
+            });
+        cy.get(exchangeRateCardRealRate).should($content => {
+            const text = $content.text();
+            expect(text).to.contain('1:1.1').and.to.contain('NT$5.00').and.to.contain('AED5.50');
+        });
+        cy.get(exchangeRateCardForcedRate).should($content => {
+            const text = $content.text();
+            expect(text).to.contain('1:N/D').and.to.contain('NT$5.00').and.to.contain('AED0.00');
+        });
+        cy.get(exchangeRateCardDelete).click();
+        cy.get(exchangeRateCard).should('not.exist');
     });
 
     it('should show source amount as target / exchange rate and target as source * exchange rate', () => {
@@ -35,6 +59,14 @@ describe('exchange rate app inputs', () => {
         cy.get(targetAmount).should('have.value', '11.10');
 
         cy.get(saveCurrencies).click();
+        cy.get(exchangeRateCardRealRate).should($content => {
+            const text = $content.text();
+            expect(text).to.contain('1:1.1').and.to.contain('€10.00').and.to.contain('$11.00');
+        });
+        cy.get(exchangeRateCardForcedRate).should($content => {
+            const text = $content.text();
+            expect(text).to.contain('0.91%').and.to.contain('1:1.11').and.to.contain('€10.00').and.to.contain('$11.10');
+        });
     });
 
     it('should show error for extreme forced exchange rate and disable saving', () => {
@@ -50,11 +82,11 @@ describe('exchange rate app inputs', () => {
             .invoke('text')
             .should(text => {
                 const initialRate = getRate(text);
-                expect(initialRate).not.to.equal('1.1');
+                expect(initialRate).not.to.equal('1.1'); // Waits for new rate to be generated
             })
             .then(text => {
-                const changedRate = getRate(text);
-                cy.get(targetAmount).should('have.attr', 'placeholder', changedRate);
+                const targetValueWithChangedRate = getRate(text);
+                cy.get(targetAmount).should('have.attr', 'placeholder', targetValueWithChangedRate);
             });
     });
 });
